1dcf2e15a7499ec1bf6c2e153f0088eeef6a41d5
Giovani Harada <giovaniharada@outlook.com>
2022-06-30 02:34:34 -0300

    contando tempo

--- time-tracker/src/components/Formulario.vue
+++ time-tracker/src/components/Formulario.vue
@@ -7,15 +7,15 @@
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
                     <section>
-                        <strong>00:00</strong>
+                        <strong>{{tempoDecorrido}}</strong>
                     </section>
-                    <button class="button">
+                    <button class="button" @click="iniciarTimer">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="pararTimer">
                         <span class="icon">
                             <i class="fa fa-stop"></i>
                         </span>
@@ -31,7 +31,30 @@
 import {defineComponent} from 'vue';
 
 export default defineComponent({
-    name: 'TimeForm'
+    name: 'TimeForm',
+    data(){
+        return {
+            tempoSegundos: 0,
+            timer: 0
+        }
+    },
+    computed: {
+        tempoDecorrido(): string{
+            return new Date(this.tempoSegundos * 1000).toISOString().substr(11,8);
+        }
+    },
+    methods: {
+        iniciarTimer(){
+            console.log("timerStart");
+            this.timer = setInterval(() => {
+                this.tempoSegundos += 1;
+                console.log(this.tempoSegundos);
+            }, 1000);
+        },
+        pararTimer(){
+            clearInterval(this.timer);
+        }
+    }
 });
 
 </script>
