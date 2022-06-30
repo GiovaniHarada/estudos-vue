2c55bb1062948a5ad00156f50f31e49c15af9d64
Giovani Harada <giovaniharada@outlook.com>
2022-06-30 01:58:42 -0300

    timetracker

---
+++ time-tracker/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <side-bar/>
+    </div>
+    <div class="column is-three-quarter">
+      <time-form/>
+      
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SideBar from './components/SideBar.vue';
+import TimeForm from './components/Formulario.vue';
+
+
+export default defineComponent({
+  components: { SideBar, TimeForm },
+  name: 'App'
+});
+</script>
+
+<style>
+
+</style>
